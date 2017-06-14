3d2eff9491b4acbcea7efa8b0263b045bd700a73
chenqiu1024 <godspeed1024@gmail.com>
2017-06-14 21:15:08 +0800

    Use GPUImage;

--- XLVideoPlayer/AppDelegate.h
+++ XLVideoPlayer/AppDelegate.h
@@ -8,11 +8,17 @@
 //  博客：http://www.jianshu.com/users/edad244257e2/latest_articles
 
 #import <UIKit/UIKit.h>
+#import "GPUImage.h"
+#import "GPUImageView.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
++ (instancetype) sharedApp;
+
 @property (strong, nonatomic) UIWindow *window;
 
+@property (strong, nonatomic) GPUImageVideoCamera* videoCamera;
+@property (strong, nonatomic) GPUImageOutput<GPUImageInput>* filter;
 
 @end
 
--- XLVideoPlayer/AppDelegate.m
+++ XLVideoPlayer/AppDelegate.m
@@ -11,14 +11,25 @@
 #import "ExampleViewController.h"
 
 @interface AppDelegate ()
+{
+    
+}
+
+- (void) startRecordingVideoSegment;
 
 @end
 
+static AppDelegate* g_instance = nil;
+
 @implementation AppDelegate
 
++ (instancetype) sharedApp {
+    return g_instance;
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    g_instance = self;
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor blackColor];
@@ -26,6 +37,31 @@
     
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:[[ExampleViewController alloc] init]];
     self.window.rootViewController = nav;
+    
+    self.filter = [[GPUImageSepiaFilter alloc] init];
+    [(GPUImageSepiaFilter*)self.filter setIntensity:0.f];
+    
+    //    filter = [[GPUImageTiltShiftFilter alloc] init];
+    //    [(GPUImageTiltShiftFilter *)filter setTopFocusLevel:0.65];
+    //    [(GPUImageTiltShiftFilter *)filter setBottomFocusLevel:0.85];
+    //    [(GPUImageTiltShiftFilter *)filter setBlurSize:1.5];
+    //    [(GPUImageTiltShiftFilter *)filter setFocusFallOffRate:0.2];
+    
+    //    filter = [[GPUImageSketchFilter alloc] init];
+    //    filter = [[GPUImageColorInvertFilter alloc] init];
+    //    filter = [[GPUImageSmoothToonFilter alloc] init];
+    //    GPUImageRotationFilter *rotationFilter = [[GPUImageRotationFilter alloc] initWithRotation:kGPUImageRotateRightFlipVertical];
+    
+    //    GPUImageView *filterView = (GPUImageView *)self.gpuImageView;
+    //    GPUImageView* filterView = [[GPUImageView alloc] initWithFrame:self.view.bounds];
+    //    filterView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    //    [self.view addSubview:filterView];
+    //    filterView.fillMode = kGPUImageFillModeStretch;
+    //    filterView.fillMode = kGPUImageFillModePreserveAspectRatioAndFill;
+    //    [filter addTarget:filterView];
+    
+    // Record a movie for 10 s and store it in /Documents, visible via iTunes file sharing
+    [self startRecordingVideoSegment];
     
     return YES;
 }
@@ -50,6 +86,89 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (void) startRecordingVideoSegment {
+    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    //    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionFront];
+    //    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1280x720 cameraPosition:AVCaptureDevicePositionBack];
+    //    self.videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset1920x1080 cameraPosition:AVCaptureDevicePositionBack];
+    self.videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    self.videoCamera.horizontallyMirrorFrontFacingCamera = NO;
+    self.videoCamera.horizontallyMirrorRearFacingCamera = NO;
+    [self.videoCamera addTarget:self.filter];
+    
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyyMMdd_hhmmss";
+    NSString* fileName = [NSString stringWithFormat:@"MOV_%@.mp4", [formatter stringFromDate:[NSDate date]]];
+    NSString* pathToMovie = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0] stringByAppendingPathComponent:fileName];
+    unlink([pathToMovie UTF8String]); // If a file already exists, AVAssetWriter won't let you record new frames, so delete the old movie
+    NSURL *movieURL = [NSURL fileURLWithPath:pathToMovie];
+    GPUImageMovieWriter* movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(480.0, 640.0)];
+    movieWriter.encodingLiveVideo = YES;
+    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(640.0, 480.0)];
+    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(720.0, 1280.0)];
+    //    movieWriter = [[GPUImageMovieWriter alloc] initWithMovieURL:movieURL size:CGSizeMake(1080.0, 1920.0)];
+    [self.filter addTarget:movieWriter];
+    
+    double delayToStartRecording = 0.5;
+    dispatch_time_t startTime = dispatch_time(DISPATCH_TIME_NOW, delayToStartRecording * NSEC_PER_SEC);
+    dispatch_after(startTime, dispatch_get_main_queue(), ^(void){
+        NSLog(@"Start recording");
+        
+        self.videoCamera.audioEncodingTarget = movieWriter;
+        [self.videoCamera startCameraCapture];
+        [movieWriter startRecording];
+        
+        //        NSError *error = nil;
+        //        if (![self.videoCamera.inputCamera lockForConfiguration:&error])
+        //        {
+        //            NSLog(@"Error locking for configuration: %@", error);
+        //        }
+        //        [self.videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOn];
+        //        [self.videoCamera.inputCamera unlockForConfiguration];
+        
+        double delayInSeconds = 15.0;
+        dispatch_time_t stopTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+        dispatch_after(stopTime, dispatch_get_main_queue(), ^(void){
+            
+            [self.filter removeTarget:movieWriter];
+            self.videoCamera.audioEncodingTarget = nil;
+            [self.videoCamera stopCameraCapture];
+            [movieWriter finishRecording];
+            NSLog(@"Movie completed");
+            [self.filter removeAllTargets];
+            /*
+             ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+             if ([library videoAtPathIsCompatibleWithSavedPhotosAlbum:movieURL])
+             {
+             [library writeVideoAtPathToSavedPhotosAlbum:movieURL completionBlock:^(NSURL *assetURL, NSError *error)
+             {
+             dispatch_async(dispatch_get_main_queue(), ^{
+             
+             if (error) {
+             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Video Saving Failed"
+             delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+             [alert show];
+             } else {
+             //                             UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Video Saved" message:@"Saved To Photo Album"
+             //                                                                            delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+             //                             [alert show];
+             }
+             remove(pathToMovie.UTF8String);
+             });
+             [self startRecordingVideoSegment];
+             }];
+             }
+             /*/
+            [self startRecordingVideoSegment];
+            //*/
+            
+            //            [self.videoCamera.inputCamera lockForConfiguration:nil];
+            //            [self.videoCamera.inputCamera setTorchMode:AVCaptureTorchModeOff];
+            //            [self.videoCamera.inputCamera unlockForConfiguration];
+        });
+    });
 }
 
 @end
